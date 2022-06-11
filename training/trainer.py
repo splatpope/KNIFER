767ad94856185e975dc3e740879d604a2f20a108
Guillaume Clemens <prdrdecroly@gmail.com>
2022-06-11 21:27:11 +0200

    SR : W is pushed to cpu

--- training/trainer.py
+++ training/trainer.py
@@ -144,18 +144,19 @@ class GANTrainer():
         return loss_G.item(), loss_D.item(), loss_D_real_val, loss_D_fake_val
 
     def spectral_regularization(self):
+        # TODO : test if W should explicitly be a copy and not a view
         with torch.no_grad():
             if self.highest_sigmas is None:
                 self.highest_sigmas = {}
                 for name, m in self.DISC.named_modules():
                     if isinstance(m, nn.Conv2d):
-                        W = m.weight.data.reshape(m.weight.shape[0], -1)#.to('cpu')
+                        W = m.weight.data.reshape(m.weight.shape[0], -1).to('cpu')
                         s = torch.linalg.svdvals(W)
                         self.highest_sigmas[name] = s/max(s)
             else:
                 for name, m in self.DISC.named_modules():
                     if isinstance(m, nn.Conv2d):
-                        W = m.weight.data.reshape(m.weight.shape[0], -1)#.to('cpu')
+                        W = m.weight.data.reshape(m.weight.shape[0], -1).to('cpu')
                         U, s, V = torch.linalg.svd(W, full_matrices=False)
                         s1 = max(s)
                         s = s / s1
@@ -163,7 +164,7 @@ class GANTrainer():
                         new_s = s1 * self.highest_sigmas[name]
                         S = torch.diag(new_s)
                         W = U @ S @ V
-                        m.weight.data = W.reshape(m.weight.shape)
+                        m.weight.data = W.reshape(m.weight.shape).to(DEVICE)
 
     @classmethod
     def get_required_params(cls):
